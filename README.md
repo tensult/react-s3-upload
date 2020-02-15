fa4336c4806e373cad5f408b9705842c5101784d
Dilip Kola <dilip@tensult.com>
2020-02-15 23:26:46 +0530

    Add S3 upload with progress bar with Amplify

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## React S3 Upload using AWS Amplify
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -37,8 +39,32 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
+### AWS Amplify
+
+Follow [this](https://aws-amplify.github.io/docs/js/react) guide.
+
+**Install:** 
+```
+yarn global add @aws-amplify/cli
+amplify configure
+```
+
+**Initialize:** 
+```
+cd to-your-react-project-root-directory
+amplify init
+``` 
+
+**Add Auth:** `amplify add auth`
+
+**Add Storage:** `amplify add storage`
+
+<img alt="React S3 Upload" src="https://user-images.githubusercontent.com/33080863/74592718-1ea2a900-504a-11ea-8d80-c129bd061c13.png">
+
 ## Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+To learn about React material UI: [material-ui](https://material-ui.com/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { RefObject } from "react";
+import "./App.css";
+import logo from "./tensult-text-logo-latest.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify, { Storage } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator, S3Text } from "aws-amplify-react";
+import Button from "@material-ui/core/Button";
+import { LinearProgress } from "@material-ui/core";
+
+Amplify.configure(awsconfig);
+Storage.configure({ level: "private" });
+
+class App extends React.Component {
+  fileInput: RefObject<any>;
+  constructor(props: any) {
+    super(props);
+    this.fileInput = React.createRef();
+  }
+  progressCallback = (progress: any) => {
+    this.setState({ fileProgress: 100 * (progress.loaded / progress.total) });
+  };
+  uploadFile = () => {
+    const file = this.fileInput.current.files[0];
+    const name = file.name;
+
+    Storage.put(name, file, { progressCallback: this.progressCallback }).then(
+      () => {
+        this.setState({ file: name });
+      }
+    );
+  };
+
+  render() {
+    const fileProgress = this.state ? (this.state as any).fileProgress : 0;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App">
+            
+          <img src={logo} className="App-logo" alt="Tensult" />
+            <p> Pick a file</p>
+            <input type="file" ref={this.fileInput} />
+            <S3Text level="private" path="" />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.uploadFile}
+            >
+              Upload
+            </Button>
+            <br /> <br />
+            {fileProgress > 0 && 
+              <LinearProgress variant="determinate" value={fileProgress} />
+            }
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
